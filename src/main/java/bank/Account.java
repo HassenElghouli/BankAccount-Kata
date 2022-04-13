cf260084a94905bb4331f98e59b02947a4446910
HassenElghouli <ing.hassen@gmail.com>
2022-04-13 17:07:21 +0100

    first-commit:US1_make a deposit in my account

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,11 @@
+package bank;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    public void deposit(BigDecimal amount){
+
+    }
+
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,9 @@
+import bank.Account;
+import org.junit.jupiter.api.BeforeEach;
+
+public class AccountTest {
+    @BeforeEach
+    void setUp(){
+        Account account = new Account();
+    }
+}
